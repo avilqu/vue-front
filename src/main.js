9738b1f1e812ba24a09637b688794c19e9497ab2
Adri Vilba <avilqu@gmail.com>
2022-05-04 23:54:28 +1200

    changed background

--- src/main.js
+++ src/main.js
@@ -4,7 +4,5 @@ import router from '@/router';
 import store from '@/store';
 import 'bootstrap';
 import './assets/css/bootstrap.min.css';
-// import './assets/css/main.css';
-// import './assets/css/login.css';
 
 createApp(App).use(store).use(router).mount('#app');
